6d8557b1d5f7340ac7409f641ea4ef47bec6324d
Alexrom2017 <alexrom2013@gmail.com>
2019-07-27 22:10:43 -0600

    Edits

---
+++ Botones.php
@@ -0,0 +1,8 @@
+<?php 
+    $arrayName = array('primary','secondary','sucess','danger','warning','info','light','dark','link' );
+    echo "hola deni";
+    for($index=0; $index < sizeof($arrayName); $index++){
+        echo '<button type="button" name="" id="" class="btn btn-'.$arrayName[$index].'" btn-lg btn-block>'.$index.'</button>';
+    }
+
+    ?>
