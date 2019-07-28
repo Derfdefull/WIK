c8d8d9182ed58eec89f0c5100c91e470b9fd85cc
Alexrom2017 <alexrom2013@gmail.com>
2019-07-28 17:06:23 -0600

    dick

--- Views/Header.php
+++ Views/Header.php
@@ -2,7 +2,7 @@
 
   <nav class="navbar navbar-expand-sm" style=" background-color: white; width:100%; heigth:10%;" >
   <a  class="navbar-brand" href="#" style="color:indigo" onMouseover="this.style.color='black'" onMouseout="this.style.color='indigo'">
-  <img width="75px" class="pl-2" src="./img/kiwi.png"/>Kiwi Dreamers </a>
+  <img width="75px" class="pl-2" src="./img/prueba.gif"/>Kiwi Dreamers </a>
   <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
