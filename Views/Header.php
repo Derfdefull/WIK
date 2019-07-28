a17a034bf193e0bef7db64add2435acff9c41458
Derfdefull <Chavez,Denilson1210@yahoo.com>
2019-07-28 00:32:22 -0600

    MENU FIX

---
+++ Views/Header.php
@@ -0,0 +1,37 @@
+  <!-- Menu -->
+  <div class="" style="border-color:indigo; width:100%; heigth:10%;">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+  <a  class="navbar-brand" href="#" style="color:indigo" onMouseover="this.style.color='black'" onMouseout="this.style.color='indigo'">
+  <img width="50px" src="./img/kiwi.png"/>Kiwi Dreamers </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="" onMouseover="this.style.color='blue'" onMouseout="this.style.color='red'">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" style="color:indigo" href="#" onMouseover="this.style.color='black'" onMouseout="this.style.color='indigo'">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color:indigo" href="#"onMouseover="this.style.color='black'" onMouseout="this.style.color='indigo'">Link</a>
+      </li>
+      <li class="nav-item dropdown" >
+        <a class="nav-link dropdown-toggle" style="color:indigo" onMouseover="this.style.color='black'" onMouseout="this.style.color='indigo'" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" onMouseover="this.style.color='black'" onMouseout="this.style.color='indigo'" aria-labelledby="navbarDropdown" style="background-color:#A675A1; "> 
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-10" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  </div>
