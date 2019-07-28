f3ab625eeb3678ff731bf46ea73aeefa648f1479
Derfdefull <Chavez,Denilson1210@yahoo.com>
2019-07-28 15:51:37 -0600

    Content

---
+++ Views/Content.php
@@ -0,0 +1,6 @@
+<div class="unit-wrapper " role="alert" style="border:solid">
+  <img style="width:40%; position:relative; left:25%; rigth:20%;" src="./img/lel.png" alt="kiwi">
+  <h4 class="alert-heading"></h4>
+  <p></p>
+  <p class="mb-0"></p>
+</div>
