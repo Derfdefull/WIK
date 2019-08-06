51853130bbadcfdeba25f275611e555b7d734451
Derfdefull <Chavez,Denilson1210@yahoo.com>
2019-08-05 19:06:43 -0600

    Favicon

--- Views/Template.php
+++ Views/Template.php
@@ -9,9 +9,11 @@
   <meta http-equiv="x-ua-compatible" content="ie-edge">
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- Favicon KiwiDreamers -->
+  <link rel="icon" href="./img/Favicon.png">
 </head>
 
-<body class="font-weight-bold">
+<body class="font-weight-bold ">
   <div class="row">
     <!--      Header    -->
     <div class="col-12">
