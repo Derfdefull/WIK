59604d449966bfbd1b2e7b8ce3b0a136917336bd
Alexrom2017 <alexrom2013@gmail.com>
2019-08-21 21:45:53 -0600

    Lel

--- Views/Content.php
+++ Views/Content.php
@@ -1,6 +1,51 @@
 <?php include_once './Source/Styles.php'; ?>
 
-<!--     Contenido Superior     -->
+<!-- Slider de info -->
+
+<div class="container">
+  <h2>Carousel Example</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="./img/LogoKIWI.png" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="./img/LogoKIWI.png" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="./img/LogoKIWI.png" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<!-- ******************************************************************************************************** -->
+
+
+
+
+
+<!--     Contenido Superior     
 <div class="row border-top">
 	<div class="col-md-4 col-sm-4 d-none d-md-block">
 
@@ -25,8 +70,8 @@
 			<br>
 		</div>
 	</div>
-</div>
-<!--      Tarjetas     -->
+</div>-->
+<!--      Tarjetas     
 
 <div class="row card-group">
 	<?php
@@ -51,4 +96,4 @@
 			</div>
 		</div>
 	<?php } ?>
-</div>
+</div>--> 
