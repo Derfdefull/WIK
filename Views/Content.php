621b5bc9e8522d52745066a26ed5f8f9503e35f5
Alexander <alexrom2013@gmail.com>
2019-09-23 20:53:42 -0600

    prueba

--- Views/Content.php
+++ Views/Content.php
@@ -1,7 +1,12 @@
-
     
     <div class="row">
-        <div class="col-4"> kkkk </div>
-        <div class="col-4"> kkkk </div>
-        <div class="col-4"> kkkk </div>
+        <div class=" col-xl-5 col-xs-12 col-sm-3 col-md-3 col-lg-4 ContentSlider1"> kkkk 
+
+        </div>
+        <div class=" col-xl-3 col-xs-12 col-sm-3 col-md-3 col-lg-4 ContentSlider2"> kkkk 
+
+        </div>
+        <div class=" col-xl-3 col-xs-12 col-sm-3 col-md-3 col-lg-4 ContentSlider3"> kkkk 
+
+        </div>
     </div>
