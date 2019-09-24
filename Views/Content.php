944228b3634751662b7de48c1a8c2496fd5d1adc
Alexander <alexrom2013@gmail.com>
2019-09-23 22:51:28 -0600

    nepe

--- Views/Content.php
+++ Views/Content.php
@@ -1,12 +1,30 @@
     
-    <div class="row">
-        <div class=" col-xl-5 col-xs-12 col-sm-3 col-md-3 col-lg-4 ContentSlider1"> kkkk 
+   <div class="row">
+        <div class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-5 ContentSlider1"> 
+        <div class="slider">
+			<ul>
+				<li>
+            <img src="./img/Ekisde.jpg" alt="">
+            </li>
+				<li>
+            <img src="http://youghaltennisclub.ie/wp-content/uploads/2014/06/Tennis-Wallpaper-High-Definition-700x300.jpg" alt="">
+            </li>
+                            <li>
+            <img src="http://welltechnically.com/wp-content/uploads/2013/08/android-wallpapers-700x300.jpg" alt="">
+            </li>
+                            <li>
+            <img src="http://welltechnically.com/wp-content/uploads/2013/09/android-widescreen-wallpaper-14165-hd-wallpapers-700x300.jpg" alt="">
+            </li>
+			</ul>
+        </div>
+        <br>
+        <button class="button button2">Cuentame mas!</button>
 
         </div>
-        <div class=" col-xl-3 col-xs-12 col-sm-3 col-md-3 col-lg-4 ContentSlider2"> kkkk 
+        <div class="col-4 col-sm-12 col-md-12 col-lg-3 col-xl-3 ContentSlider2"> kkkk 
 
         </div>
-        <div class=" col-xl-3 col-xs-12 col-sm-3 col-md-3 col-lg-4 ContentSlider3"> kkkk 
+        <div class="col-4 col-sm-12 col-md-12 col-lg-4 col-xl-3 ContentSlider3"> kkkk 
 
         </div>
     </div>
