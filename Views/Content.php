e9b226e555d6b4213e1fa41e87b5a9d76f5889ee
Derfdefull <Chavez.Denilson1210@yahoo.com>
2019-09-23 23:54:51 -0600

    margin boxes

--- Views/Content.php
+++ Views/Content.php
@@ -1,6 +1,6 @@
     
    <div class="row">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-5 ContentSlider1"> 
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-5 mr-3 mb-3 ContentSlider1"> 
         <div class="slider">
 			<ul>
 				<li>
@@ -18,13 +18,13 @@
 			</ul>
         </div>
         <br>
-        <button class="button button2">Cuentame mas!</button>
+        <button class="button button2"> ¡Cuentame mas! </button>
 
         </div>
-        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 ContentSlider2"> kkkk 
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 mr-3 mb-3 ContentSlider2"> kkkk 
 
         </div>
-        <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-3 ContentSlider3"> kkkk 
+        <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-3 mr-3 mb-3 ContentSlider3"> kkkk 
 
         </div>
     </div>
